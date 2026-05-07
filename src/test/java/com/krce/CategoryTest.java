e5d0e463d0b86c2b655369721fd23a8d93ed756c
pavithra <pavithrapanneer26@gmail.com>
2026-05-07 11:20:57 +0530

    added

--- src/test/java/com/krce/CategoryTest.java
+++ src/test/java/com/krce/CategoryTest.java
@@ -1,4 +1,65 @@
 package com.krce;
 
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.hamcrest.Matchers;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.Map;
+
+import static io.restassured.RestAssured.*;
+
 public class CategoryTest {
+    private int id;
+   @BeforeClass
+    public void Setup(){
+       RestAssured.baseURI="https://api.escuelajs.co/api/v1";
+    }
+    @Test(priority = 1)
+    public void testCreateCategory(){
+       String name= "user_"+System.currentTimeMillis();
+       String image="https://google.com";
+       Map body=Map.of("name",name,"image",image);
+       Response response =RestAssured.given()
+               .contentType(ContentType.JSON)
+               .body(body)
+               .when()
+               .post("/categories");
+       response
+        .then()
+                .log().all()
+                .statusCode(201)
+                .body("name", Matchers.equalTo(name));
+       id=response.jsonPath().getInt("id");
+    }
+    @Test(priority = 2)
+    public void testGetCategory(){
+       RestAssured.given()
+               .pathParam("id",id)
+               .when()
+                .get("/categories/{id}")
+                .then()
+                .log().all()
+                .statusCode(200)
+                .body("id",Matchers.equalTo(id));
+    }
+    @Test(priority=3)
+    public void TestUpdateCategory(){
+       String name="category_"+System.currentTimeMillis();
+       String image="https//google.com";
+       RestAssured.given()
+               .pathParam("id",id)
+.body(       .when()
+               .put("/categories/{id}")
+               .then()
+                .log().all()
+                .statusCode(200)
+                .body("id",Matchers.equalTo(name));
+    }
 }
+
+
+
--- src/test/java/com/krce/FakeApitest.java
+++ src/test/java/com/krce/FakeApitest.java
@@ -1,4 +1,64 @@
 package com.krce;
 
+import io.restassured.RestAssured;
+import org.hamcrest.Matchers;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
 public class FakeApitest {
+    @BeforeClass
+    public void setup(){
+        RestAssured.baseURI="https://api.escuelajs.co/api/v1";
+    }
+    @Test
+    public void TestGetProducts(){
+        RestAssured.given()
+                .when()
+                .get("/products")
+                .then()
+                .statusCode(200)
+                .body("size()", Matchers.greaterThan(0));
+    }
+    @Test
+    public void testfilterProductsByPrice(){
+        RestAssured.given()
+                .queryParam("price",100)
+                .when()
+                .get("/products/")
+                .then()
+                .statusCode(200)
+                .body("[0].price",Matchers.equalTo(100));
+    }
+    @Test
+    public void testGetCategories(){
+        RestAssured.given()
+                .when()
+                .get("/categories")
+                .then()
+                .statusCode(200)
+                .body("$",Matchers.instanceOf(List.class));
+    }
+    @Test
+    public void testGetCategoriesById(){
+
+        RestAssured.given()
+                .pathParam("id",1)
+                .when()
+                .get("/categories/{id}")
+                .then()
+                .statusCode(200)
+                .body("id",Matchers.equalTo(1));
+    }
+    public void testgetCategories(){
+        RestAssured.given()
+                .when()
+                .get("/categories")
+                .then()
+                .statusCode(200)
+                .body("$",Matchers.instanceOf(List.class));
+    }
+
+
 }
